87589d5638f093e6c3c71a0823f2f3c154a7495c
liwen <hellocreep@gmail.com>
2013-04-02 15:50:59 +0800

    ..

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,20 @@
 $(function(){
 
 	$( '.section' ).not( '.section-below' ).hover(function(){
-		$( this ).children( '.desc' ).animate({
-			left: '400px'
-		},1000);
-
+		$( this ).children( '.desc' )
+		.filter(':not(:animated)').animate({
+			left: '20px'
+		}, 1000);
+		// $( this ).children( '.desc' ).stop(true, false).animate({
+		// 	left: '20px'
+		// }, 1000);
 	}, function(){
 		$( this ).children( '.desc' ).animate({
 			left: '-400px'
-		},1000);
+		}, 1000);
+		// $( this ).children( '.desc' ).stop(true, false).animate({
+		// 	left: '-400px'
+		// }, 1000);
 	}).click(function(){
 		window.location.href = window.location.href + $( this ).data( 'target' );
 	});
