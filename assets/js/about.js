de720bf74722ac7e68dd5fffcf46b1a4dd7bb3cb
liwen <hellocreep@gmail.com>
2013-04-08 23:35:35 +0800

    add local folder

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,9 +1,35 @@
 (function($){
+
+	var resize = function(){
+		var w_height = $( window ).height(),
+			w_width = $( window ).width()
+
+		$( '#about' ).css({
+			height: $( window ).height()
+		}).fadeIn();
+
+		var path_menu = $( '#path_menu' );
+		path_menu.css({
+			left: (w_width - path_menu.width()) / 2
+		}).animate({
+			top: (w_height - path_menu.height()) / 2,
+			opacity: 1
+		}, 1500);
+	}
+
+	var path = function() {
+		$( '#avatar' ).click(function(){
+
+		});
+	}
+
 $(function(){
 
-	$( '#about' ).css({
-		height: $( window ).height()
-	}).fadeIn('fast');
+	resize();
+
+	$( window ).resize(function(){
+		resize();
+	});
 
 })
 })(jQuery);
