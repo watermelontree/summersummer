38c2e9a126e91b0a227c0d4ae86e4e3fd3cadb3e
liwen <hellocreep@gmail.com>
2013-04-13 00:08:21 +0800

    about page

--- assets/js/about.js
+++ assets/js/about.js
@@ -19,11 +19,51 @@
 
 	var path = function() {
 		$( '#avatar' ).click(function(){
+			var $this = $( this );
+			if( $this.hasClass('rollin') ) {
+				$this.removeClass('rollin').addClass('rollout');
+			}else{
+				$this.removeClass('rollout').addClass('rollin');
+			}
 
+			$( '.collect' ).find( '.item' ).each(function(index){
+				var left, top;
+				switch(index){
+					case 0:
+						left = '-15px';
+						top = 40;
+						break;
+					case 1:
+						left = 50;
+						top = 80;
+						break;
+					case 2:
+						left = 120;
+						top = 40;
+						break;
+				}
+				var $this = $( this );
+				if( $this.hasClass('rollin') ) {
+					$this.removeClass('rollin').addClass('rollout')
+					.animate({
+						top: '-='+top,
+						left: 50
+					});
+				} else {
+					$this.removeClass('rollout').addClass('rollin')
+					.animate({
+						top: top,
+						left: left
+					});
+				}
+				
+			});
 		});
 	}
 
 $(function(){
+
+	path();
 
 	resize();
 
